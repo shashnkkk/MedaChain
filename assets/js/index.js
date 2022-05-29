2d9789ae43ea05b0082b57b4e5efd521fe46af8a
Piyush <90107289+PiyushKarmhe@users.noreply.github.com>
2022-05-30 00:35:54 +0530

    login automated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+const login_signin = document.querySelector("#Dash_Board");
 
 if (typeof window.ethereum !== 'undefined') {
     window.web3 = new Web3( window.ethereum);
@@ -607,10 +608,35 @@ web3.eth.getAccounts().then(function(accounts) {
 	console.log(account);
 });
 
-contract.methods.Is_present(account).send({from: account }, function (err, res) {
-	if (err) {
-		console.log("An error occured", err)
-		return
-	}
-	console.log()
-});
+const signin = ()=>{
+    contract.methods.Is_present(account).call({from: account }, function (err, res) {
+        if (err) {
+            console.log("An error occured", err)
+            return
+        }
+        console.log(res);
+        if(res==true){
+            contract.methods.person_type(account).call({from: account }, function (err, _type) {
+                if (err) {
+                    console.log("An error occured", err);
+                    return;
+                }
+                console.log(_type);
+                if(_type === "doctor"){
+                    window.location.href = `./doctor.html`;
+                }
+                else if(_type === "patient"){
+                    window.location.href = `./patient.html`;
+                }
+                else{
+                    window.location.href = `./agent.html`;
+                }
+            });
+        }
+        else{
+            window.location.href = `./registration.html`;
+        }
+    });
+};
+
+login_signin.addEventListener("click",signin);
