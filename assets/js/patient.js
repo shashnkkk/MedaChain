7c07c374e1a014dd3d730808239060e42b6deaef
Piyush <90107289+PiyushKarmhe@users.noreply.github.com>
2022-05-30 05:37:40 +0530

    partially working patients portal

--- assets/js/patient.js
+++ assets/js/patient.js
@@ -1,14 +1,26 @@
 const submit = document.querySelector("#submit");
+const load = document.querySelector("#load");
 
-const download_url = async ()=>{
+
+const download_url = ()=>{
     contract.methods.download_prep(account).call({from: account }, function (err, result) {
         if (err) {
             console.log("An error occured", err);
             return;
         }
-
+        console.log("Checking");
     });
-    // console.log(count);
 }
 
+const load_img = ()=>{
+    contract.methods.urls_return().call({from: account }, function (err, result) {
+        if (err) {
+            console.log("An error occured", err);
+            return;
+        }
+        console.log("loading");
+        console.log(result);
+    });
+}
 submit.addEventListener("click",download_url);
+load.addEventListener("click",load_img);
